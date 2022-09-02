09ad102db595ad2c5e8014d382f1dcbe544c6aa8
Kate Mahova <kate.mahova@yandex.ru>
2022-09-02 21:58:17 +0300

    feature: item requests, testing. v14

--- src/test/java/ru/practicum/shareit/item/ItemsIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/item/ItemsIntegrationTest.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit.item;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.user.User;
