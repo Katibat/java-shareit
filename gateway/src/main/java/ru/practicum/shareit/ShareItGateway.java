6554de7e3b7ddfcc5dbf8a9a80f08ce9b0476aec
Kate Mahova <kate.mahova@yandex.ru>
2022-09-14 14:34:42 +0300

    fix: codestyle 2

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItGateway {
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItGateway.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItGateway.class, args);
+    }
 }
--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItServer {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItServer.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItServer.class, args);
+    }
 }
